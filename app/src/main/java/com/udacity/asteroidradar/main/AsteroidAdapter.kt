c0d73057413f4bd3236d33f53aa4610f548fb51a
Rania Makki <rmakki@gmail.com>
2021-10-05 13:22:44 -0400

    Modified Adapter to use AsteroidAdapter ViewHolder

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -2,7 +2,9 @@ package com.udacity.asteroidradar.main
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.Asteroid
@@ -10,7 +12,7 @@ import com.udacity.asteroidradar.Util
 import com.udacity.asteroidradar.R
 
 
-class AsteroidAdapter: RecyclerView.Adapter<Util.TextItemViewHolder>() {
+class AsteroidAdapter: RecyclerView.Adapter<AsteroidAdapter.ViewHolder>() {
 
      var data = listOf <Asteroid>()
         set(value) {
@@ -21,16 +23,24 @@ class AsteroidAdapter: RecyclerView.Adapter<Util.TextItemViewHolder>() {
         return data.size
     }
 
-    override fun onBindViewHolder(holder: Util.TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.codename
+        holder.asteroid_id.text = item.codename
         Log.i("Recyle item", item.codename)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Util.TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
-            .inflate(R.layout.text_item_view, parent, false) as TextView
-        return Util.TextItemViewHolder(view)    }
+            .inflate(R.layout.list_item_asteroid, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val asteroid_id: TextView = itemView.findViewById(R.id.asteroid_id)
+        val asteroid_date_approaching: TextView = itemView.findViewById(R.id.asteroid_date_approaching)
+        val asteroid_status_image: ImageView = itemView.findViewById(R.id.status_image)
+    }
 
 }
